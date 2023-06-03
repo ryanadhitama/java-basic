c7da5c28bcdce238ac12a5ebdef2d4bc56a78048
ryanadhitama <ryanadhitama2@gmail.com>
2023-06-04 02:05:36 +0800

    number

---
+++ src/Number.java
@@ -0,0 +1,13 @@
+public class Number {
+    public static void main(String[] args) {
+        byte thisByte = 100; // -128 - 127
+        short thisShort = 1000; // -32768 - 32767
+        int thisInt = 1200; // -2147483648 - 2147483647
+        long thisLong = 12000; // -9223372036854775808 - 9223372036854775807
+
+        System.out.println(thisByte);
+        System.out.println(thisShort);
+        System.out.println(thisInt);
+        System.out.println(thisLong);
+    }
+}
